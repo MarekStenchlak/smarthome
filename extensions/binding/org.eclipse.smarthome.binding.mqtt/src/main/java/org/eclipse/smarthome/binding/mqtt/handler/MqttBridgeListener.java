60d6344a1b6d85eeb4d58d0fa316570985346c1e
Marcel Verpaalen <marcel@verpaalen.com>
2015-12-19 00:42:10 +0100

    testversion mqtt

---
+++ extensions/binding/org.eclipse.smarthome.binding.mqtt/src/main/java/org/eclipse/smarthome/binding/mqtt/handler/MqttBridgeListener.java
@@ -0,0 +1,12 @@
+package org.eclipse.smarthome.binding.mqtt.handler;
+
+/**
+ * The {@link MqttBridgeListener} is responsible for transmitting events from MQTT bridge to all it's associated Topics.
+ *
+ * @author Marcus of Wetware Labs - Initial contribution
+ */
+public interface MqttBridgeListener {
+
+    // Stub interface for future use
+
+}
---
+++ extensions/binding/org.eclipse.smarthome.binding.mqtt/src/main/java/org/eclipse/smarthome/binding/mqtt/internal/MqttMessageSubscriberListener.java
@@ -0,0 +1,29 @@
+/**
+ *
+ */
+package org.eclipse.smarthome.binding.mqtt.internal;
+
+/**
+ * Callback for received MQTT messages for the clients of MqttMessageSubscriber
+ *
+ * @author Marcus of Wetware Labs
+ *
+ */
+public interface MqttMessageSubscriberListener {
+
+    /***
+     * Received a MQTT message with a topic defined as Command
+     * 
+     * @param topic
+     * @param command
+     */
+    public abstract void mqttCommandReceived(String topic, String command);
+
+    /***
+     * Received a MQTT message with a topic defined as State
+     * 
+     * @param topic
+     * @param command
+     */
+    public abstract void mqttStateReceived(String topic, String state);
+}
