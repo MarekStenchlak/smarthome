47fa669a9b04185faee63954b40c537b47e0d407
Marcel Verpaalen <marcel@verpaalen.com>
2016-01-08 00:46:39 +0100

    additional mqtt config options configurable discovery topic
    Signed-off-by: Marcel Verpaalen <marcel@verpaalen.com>

--- extensions/binding/org.eclipse.smarthome.binding.mqtt/src/main/java/org/eclipse/smarthome/binding/mqtt/discovery/MqttDiscoveryService.java
+++ extensions/binding/org.eclipse.smarthome.binding.mqtt/src/main/java/org/eclipse/smarthome/binding/mqtt/discovery/MqttDiscoveryService.java
@@ -51,6 +51,10 @@ public class MqttDiscoveryService extends AbstractDiscoveryService
 
     private Thing bridge;
 
+    private String discoveryTopic = "#";
+
+    private String discoveryMode = "MULTIPLE";
+
     private final static Logger logger = LoggerFactory.getLogger(MqttDiscoveryService.class);
 
     public MqttDiscoveryService(MqttBridgeHandler mqttBridgeHandler, Thing thing) {
@@ -58,6 +62,16 @@ public class MqttDiscoveryService extends AbstractDiscoveryService
         // super(MqttHandler.SUPPORTED_THING_TYPES, 10, true);
         this.bridgeHandler = mqttBridgeHandler;
         this.bridge = thing;
+    }
+
+    public MqttDiscoveryService(MqttBridgeHandler mqttBridgeHandler, Thing thing, String discoveryTopic,
+            String discoveryMode) {
+        super(10);
+        // super(MqttHandler.SUPPORTED_THING_TYPES, 10, true);
+        this.bridgeHandler = mqttBridgeHandler;
+        this.bridge = thing;
+        this.discoveryTopic = discoveryTopic;
+        this.discoveryMode = discoveryMode;
     }
 
     @Override
@@ -84,7 +98,10 @@ public class MqttDiscoveryService extends AbstractDiscoveryService
 
     @Override
     public void deactivate() {
-        bridgeHandler.unRegisterMessageConsumer(subscriber);
+        if (bridgeHandler != null) {
+            bridgeHandler.unRegisterMessageConsumer(subscriber);
+            bridgeHandler.unRegisterMqttBridgeListener(this);
+        }
         discoveredTopics.clear();
     }
 
@@ -100,9 +117,11 @@ public class MqttDiscoveryService extends AbstractDiscoveryService
 
                 subscriber = new MqttMessageSubscriber(
                         // getBridgeHandler().getUID().getId() + ":" + topic + ":" + type + ":" + transform, this);
-                        bridgeHandler.getBroker() + ":" + "#" + ":" + "state" + ":" + "default", this);
+                        bridgeHandler.getBroker() + ":" + discoveryTopic + ":" + "state" + ":" + "default", this);
 
                 bridgeHandler.registerMessageConsumer(subscriber);
+                bridgeHandler.registerMqttBridgeListener(this);
+
                 logger.error("Registered discovery subscriber for broker: {}", bridgeHandler.getBroker());
 
             } catch (Exception e) {
@@ -130,23 +149,38 @@ public class MqttDiscoveryService extends AbstractDiscoveryService
     public void mqttStateReceived(String topic, String state) {
         logger.trace("MQTT topic discovery: Received state (topic '{}' payload '{}')", topic, state.toString());
 
-        if (!discoveredTopics.contains(topic)) {
-            String id = makeTopicString(topic);
-
-            logger.trace("Adding new topic thing on {} with id '{}' to Smarthome inbox", topic, id);
-            Map<String, Object> properties = new HashMap<>(2);
-            properties.put(TOPIC_ID, topic);
-            properties.put(TYPE, "state");
-            properties.put(DIRECTION, "in");
-            properties.put(TRANSFORM, "default");
-            ThingUID uid = new ThingUID(THING_TYPE_TOPIC, id);
-            if (uid != null) {
-                DiscoveryResult result = DiscoveryResultBuilder.create(uid).withProperties(properties)
-                        .withBridge(bridge.getUID()).withLabel("topic: " + topic).build();
-                thingDiscovered(result);
+        String discoveredTopic = topic;
+        if (discoveryMode.equals("MULTIPLE")) {
+            String[] result = topic.split("/");
+            if (result.length > 2) {
+                result[result.length - 1] = "#";
             }
-            discoveredTopics.add(topic);
+            discoveredTopic = StringUtils.join(result, "/");
         }
+        if (!discoveredTopics.contains(discoveredTopic)) {
+            addDiscoveryResult(discoveredTopic);
+        }
+    }
+
+    /**
+     * @param topic
+     */
+    private void addDiscoveryResult(String topic) {
+        String id = makeTopicString(topic);
+
+        logger.trace("Adding new topic thing on {} with id '{}' to Smarthome inbox", topic, id);
+        Map<String, Object> properties = new HashMap<>(2);
+        properties.put(TOPIC_ID, topic);
+        properties.put(TYPE, "state");
+        properties.put(DIRECTION, "in");
+        properties.put(TRANSFORM, "default");
+        ThingUID uid = new ThingUID(THING_TYPE_TOPIC, id);
+        if (uid != null) {
+            DiscoveryResult result = DiscoveryResultBuilder.create(uid).withProperties(properties)
+                    .withBridge(bridge.getUID()).withLabel("topic: " + topic).build();
+            thingDiscovered(result);
+        }
+        discoveredTopics.add(topic);
     }
 
     private String makeTopicString(String topicString) {
@@ -160,7 +194,7 @@ public class MqttDiscoveryService extends AbstractDiscoveryService
             result[i] = capitalize(result[i]);
         }
 
-        String resulttopic = StringUtils.join(result, "");
+        String resulttopic = StringUtils.join(result, "").replace("#", "");
         return resulttopic;
     }
 
@@ -171,6 +205,16 @@ public class MqttDiscoveryService extends AbstractDiscoveryService
         char c[] = string.toCharArray();
         c[0] = Character.toUpperCase(c[0]);
         return new String(c);
+    }
+
+    @Override
+    public void discoveryConfigUpdate(String discoveryTopic, String discoveryMode) {
+        this.discoveryTopic = discoveryTopic;
+        this.discoveryMode = discoveryMode;
+        if (bridgeHandler != null) {
+            bridgeHandler.unRegisterMessageConsumer(subscriber);
+        }
+        subscribe();
     }
 
 }
